8ce294e268dfa4375bbf386b222286adb20582b7
Keenan Weise <kcweise@gmail.com>
2024-02-20 05:11:21 -0600

    Functions continued lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function(activity="go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+
+const wrapAdjective = function(wrap="*"){
+    return function(special = "special"){
+        return `You are ${wrap}${special}${wrap}!`
+    }
+}
